dcf17c4307505d839c7dfaf22eb83061cfa99081
Subash Paudel <subashpaudel144@gmail.com>
2021-07-17 10:09:45 +0545

    landing page

---
+++ client/src/components/Personal.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./css/Personal.css";
+
+export class Personal extends Component {
+  render() {
+    return (
+      <div className="last">
+        <div className="desc">
+          <h1>RANJZIT GURUNG</h1>
+          <p>
+            I am Ranjzit Gutung from syangjya and I am currently working on
+            shooting vidoes and photographs.
+            <br />
+            If anyone wants quality video and photographer for any ceremony like
+            marriage, bratabanda, puja...etc,
+            <br />
+            You can easily contact me. To contact press on the contact button
+            above .<br /> You can directly call me or also you can directly
+            message me on facebook or also you can email me on the given
+            address.
+          </p>
+        </div>
+        <div className='footer'>
+        <p>
+          @Copyright: Ranjzit Gurung.. Quality videographer and
+          photographer.....
+        </p>
+        
+      </div>
+    </div>
+     
+    );
+  }
+}
+
+export default Personal;
