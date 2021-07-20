95471c25a073dd6583316b2fa17a50b6e0190bec
Subash Paudel <subashpaudel144@gmail.com>
2021-07-20 17:41:06 +0545

    marriage and nature component

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import Address from './Address';
+import './css/Gallery.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Marriage from './Marriage';
+import Nature from './Nature';
 
 export class Gallery extends Component {
     render() {
         return (
-            <div>
-                <Address /> <Address />
-                
+            <div className='gallery'>
+                <Router>
+                <div className='top'>
+                    <h1>HELLO WELCOME TO RANJZIT PHOTO GALLERY<br/> WITH CREATIVE AND UNIQUE STYLE</h1>
+                    <a href="/gallery/marriage">Marriage</a>
+                    <a href="/gallery/nature">Nature</a>
+                </div>
+                <Switch>
+                    <Route path='/gallery/marriage' exact component={ Marriage }/>
+                    <Route path='/gallery/nature' exact component={ Nature }/>
+                </Switch>
+                </Router>
             </div>
         )
     }
